5678d0bdccf82690a6d1cb10a87ee27ede7803ee
Pavla Vašková <d23147@oa-opava.cz>
2026-06-18 09:16:24 +0200

    jnkj

--- traveling/settings.py
+++ traveling/settings.py
@@ -127,7 +127,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# Toto přidej, pokud to tam nemáš:
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
